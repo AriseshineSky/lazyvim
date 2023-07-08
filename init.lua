d97633aa9054c0e1d4bfa6e7d47550b9ae0393d0
SmileintheSky <befruitful12@gmail.com>
2023-07-08 15:48:45 -0500

    change keymap

--- init.lua
+++ init.lua
@@ -211,26 +211,6 @@ require("nvim-treesitter.configs").setup({
 	},
 })
 -- Lua
-keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
-  {silent = true, noremap = true}
-)
-keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
-keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
-keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
-  {silent = true, noremap = true}
-)
-keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-  {silent = true, noremap = true}
-)
-keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-  {silent = true, noremap = true}
-)
-
-vim.api.nvim_command('autocmd FileType javascript setlocal shiftwidth=2 tabstop=2 expandtab')
 
 
 require("lspconfig").pyright.setup({
